ada99b97ef15074ba841cb890a12c7b382686997
AbhijeetPatel19 <abhijeetpatel.phys@gmail.com>
2025-04-27 14:06:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,9 +13,10 @@ input_channels = 3
 num_classes = 10  # for 10 sea animal species
 
 # Paths (Kaggle specific)
-#data_dir = '/kaggle/input/sea-animals-ds/DatasetsCNN'
+#data_dir = '/kaggle/input/sea-animals-ds/DatasetsCNN' IF YOU WANT TO TRAIN(DatasetsCNN should have training, testing and validation subfolders)
 
 # Update with your dataset path
 
 checkpoint_path = '/kaggle/working/project_Abhijeet_Patel/checkpoints/final_weights.pth'
 test_dir = './data'
+#data_dir = test_dir IF YOU WANT TO JUST PREDICT AND NOT TRAIN
